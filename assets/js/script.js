5622a7acbe8fd76a5a1018dc35f36fc4550fca6b
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-04-24 19:27:31 -0700

    Changes to be committed:

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var today = dayjs()
+
+var timeBlock = doucment.querySelector(".container");
+
+$(function () {
+  $("#currentDateElement").text(today.format(" dddd, MMMM DD, YYYY, h:mm a"));
+  console.log(currentDate);
+
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+  });
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+function auditTask() {
+  var currentHour = today.hour();
+
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (timeId < currentHour) {
+      $(this).addClass("future");
+    }
+
+    else if (timeId === currentHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    }
+
+    else {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    }
+  })};
+
+  auditTask();
+
+  setTimeout(function () {
+    location = "";
+  }, 1000 * 60);
+
+});
